426837c2b88bc332dd180a901d0437859b5352c1
Willian-code <willianrosendodelima@outlook.com>
2022-10-03 14:23:18 -0300

    creating a method and layout to insert new posts

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -4,11 +4,15 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -33,5 +37,20 @@ public class CodeBlogController {
 
         mv.addObject("posts", posts);
         return mv;
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDateTime.now());
+        codeBlogService.save(post);
+        return "redirect:/posts";
     }
 }
