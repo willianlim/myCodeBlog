02b6ed6ffb812de2adfdb6edd44fde7a04c02023
Willian-code <willianrosendodelima@outlook.com>
2022-10-02 21:07:32 -0300

    application that mediates communication between a controller and repository layer

---
+++ src/main/java/com/spring/codeblog/service/serviceImpl/CodeBlogServiceImpl.java
@@ -0,0 +1,31 @@
+package com.spring.codeblog.service.serviceImpl;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeBlogRepository;
+import com.spring.codeblog.service.CodeBlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CodeBlogServiceImpl implements CodeBlogService {
+
+    @Autowired
+    CodeBlogRepository codeBlogRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return codeBlogRepository.findAll();
+    }
+
+    @Override
+    public Post findById(long id) {
+        return codeBlogRepository.findById(id).get();
+    }
+
+    @Override
+    public Post save(Post post) {
+        return codeBlogRepository.save(post);
+    }
+}
