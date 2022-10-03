9b36e56d00dec355bcbcfb5ae434c4acadf2c617
Willian-code <willianrosendodelima@outlook.com>
2022-10-02 21:34:56 -0300

    implementing the 'GET' method

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -1,2 +1,27 @@
-package com.spring.codeblog.controller;public class CodeBlogController {
+package com.spring.codeblog.controller;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.service.CodeBlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CodeBlogController {
+
+    @Autowired
+    CodeBlogService codeBlogService;
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts() {
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeBlogService.findAll();
+
+        mv.addObject("posts", posts);
+        return mv;
+    }
 }
