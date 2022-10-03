8a9e104dc6071ac582b8c40cfbb9184b357bd161
Willian-code <willianrosendodelima@outlook.com>
2022-10-03 10:48:24 -0300

    specific method to return a post by passing an id

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,6 +21,15 @@ public class CodeBlogController {
     public ModelAndView getPosts() {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeBlogService.findAll();
+
+        mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post posts = codeBlogService.findById(id);
 
         mv.addObject("posts", posts);
         return mv;
